899fa0e855836dcc8a9b341956edf4ba59740eeb
임찬형 <ich0906@naver.com>
2022-11-03 15:54:11 +0900

    마이페이지 및 리뷰 ui 작성

--- app/src/main/java/com/ich/whereistoilet/domain/model/ReviewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.ich.whereistoilet.domain.model
-
-data class ReviewModel(
-    val key: String = "",
-    val userId: String = "",
-    val toiletId: Int = 0,
-    val title: String = "",
-    val content: String = ""
-)
